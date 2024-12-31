610922627b69ecc5814413e3e73d9896048f2c27
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-31 15:15:06 -0800

    Fixes

--- hosts/aarch64/MBP-M1-VM.nix
+++ hosts/aarch64/MBP-M1-VM.nix
@@ -1,22 +1,101 @@
 { pkgs, lib, config, ... }:
 {
-  # Disk information
+  ##### Disk Information #####
   variables.disk.device = "/dev/nvme0n1";
   variables.disk.swapSize = "1G"; # Small swap for a VM
-  # VM
-  boot.initrd.availableKernelModules = [ "virtio_pci" "xhci_pci" "usb_storage" "usbhid" ];
-  # ARM64 Packages
+
+  ##### Boot Configuration #####
+  boot = {
+    # Include necessary kernel modules for QEMU / KVM virtual machines
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "nvme"
+        "sr_mod"
+        "virtio_pci"
+        "virtio_blk"
+        "virtio_scsi"
+        "virtio_net"
+        "virtio_gpu"
+        "virtiofs"
+        "drm"
+        "drm_kms_helper"
+        "fbcon"
+        "usbhid"
+        # Audio modules (uncomment if needed)
+        # "snd_hda_intel"
+        # "snd_hda_codec"
+      ];
+      framebuffer = true;
+    };
+    kernelModules = [
+      "virtio_balloon"
+    ];
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 3;
+        consoleMode = "max";
+        editor = false;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+    kernelParams = [
+      "quiet"
+      "splash"
+      "loglevel=3"                  # Reduce kernel log verbosity
+      "vt.global_cursor_default=0"  # Hide the console cursor during boot
+      "rd.udev.log_priority=3"      # Minimize udev messages
+      "systemd.show_status=auto"    # Hide systemd boot status unless there's an error
+      "rd.systemd.show_status=auto" # Same as above, but for the initrd phase
+    ];
+    # Use the latest kernel packages
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  ##### Hardware Configuration #####
+  # GPU acceleration
+  hardware.opengl.enable = true;
+
+  # Explicitly disable Parallels support
+  hardware.parallels.enable = false;
+
+  ##### Services #####
+  # Enable qemu guest agent
+  services = {
+    qemuGuest.enable = true;
+    spice-vdagentd.enable = true; # For clipboard sharing with Spice
+    # Other services can be added here
+  };
+
+  # Enable spice-vdagent program
+  programs.spice-vdagent.enable = true;
+
+  ##### Networking #####
+  networking.useDHCP = lib.mkForce true;
+
+  ##### Packages #####
+  # ARM64-specific Packages
   environment.systemPackages = with pkgs; [
     chromium
+    spice-vdagent
   ];
-  # Qemu
-  services.spice-vdagentd.enable = true;
-  services.spice-webdavd.enable = true;
-  # Parallels
-  hardware.parallels.enable = false;
-  # TODO Update to a newer kernel after Parallels updates their drivers
-  # https://github.com/NixOS/nixpkgs/issues/364391
-  boot.kernelPackages = lib.mkIf config.hardware.parallels.enable (lib.mkForce pkgs.linuxPackages_6_6);
-  # Networking
-  networking.useDHCP = lib.mkForce true;
+
+  ##### Filesystem Configurations #####
+  # Configure VirtFS mount
+  fileSystems."/mnt/shared_folder" = {
+    device = "shared_folder";  # Mount tag from UTM
+    fsType = "virtiofs";
+    options = [ "rw" "defaults" ];
+  };
+
+  # Ensure mount point exists and set permissions
+  system.activationScripts.createVirtFSMountPoint = lib.mkAfter ''
+    mkdir -p /mnt/shared_folder
+    chown ${config.variables.user.admin}:users /mnt/shared_folder
+    chmod 770 /mnt/shared_folder
+  '';
 }
