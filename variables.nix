35b6010745351d12e65a1bf75f756b76bbaedd3a
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-31 12:49:55 -0800

    Fixes

--- variables.nix
+++ variables.nix
@@ -1,12 +1,14 @@
 {lib, ...}:
-{
-  variables = {
+let
+  mkObject = object: lib.mkOption { type = lib.types.attrsOf lib.types.anything; default = object; };
+in {
+  options.variables = mkObject {
     secrets = "/etc/nixos/secrets";
     user = {
       admin = "alexanderschiffhauer";
       hashedPasswordFile = "hashed_password.txt";
     };
-    disk = {
+    disk = mkObject {
       device = "OVERRIDE_THIS_VALUE_IN_HOST";
       swapSize = "OVERRIDE_THIS_VALUE_IN_HOST";
       tmpPasswordFile = "/tmp/plain_text_password.txt";
