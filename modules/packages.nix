e0e27ad39cdeeb15271d136da6b6284854ffe441
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-30 18:53:56 -0800

    Trying out UTM again w/ Ghostty

--- modules/packages.nix
+++ modules/packages.nix
@@ -13,7 +13,7 @@
       _1password-gui
       _1password-cli
       alacritty
-      inputs.ghostty.packages.${nixpkgs.system}.default
+      inputs.ghostty.packages.${inputs.nixpkgs.system}.default
     ];
     programs._1password.enable = true;
     programs._1password-gui = {
