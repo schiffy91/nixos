0c2f60cbe3f5d7cdc8ed2754b0db79ecc36ac1c7
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-31 12:56:42 -0800

    Fixes

--- variables.nix
+++ variables.nix
@@ -1,16 +1,19 @@
 {lib, ...}:
 {
-  options.variables = lib.mkOption { type = lib.types.attrsOf lib.types.anything; default = {
-    secrets = "/etc/nixos/secrets";
-    user = {
-      admin = "alexanderschiffhauer";
-      hashedPasswordFile = "hashed_password.txt";
-    };
-    disk = {
-      device = "OVERRIDE_THIS_VALUE_IN_HOST";
-      swapSize = "OVERRIDE_THIS_VALUE_IN_HOST";
-      tmpPasswordFile = "/tmp/plain_text_password.txt";
-      pkiBundle = "/var/lib/sbctl";
+  options.variables = lib.mkOption { 
+    type = lib.types.attrsOf lib.types.anything; 
+    default = {
+      secrets = "/etc/nixos/secrets";
+      user = {
+        admin = "alexanderschiffhauer";
+        hashedPasswordFile = "hashed_password.txt";
+      };
+      disk = {
+        device = "OVERRIDE_THIS_VALUE_IN_HOST";
+        swapSize = "OVERRIDE_THIS_VALUE_IN_HOST";
+        tmpPasswordFile = "/tmp/plain_text_password.txt";
+        pkiBundle = "/var/lib/sbctl";
+      };
     };
   };
 }
