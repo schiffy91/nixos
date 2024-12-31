14f5c4c7b946e753af4371d866b2744e784eaa69
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-31 13:16:37 -0800

    Fixes

--- variables.nix
+++ variables.nix
@@ -1,19 +1,12 @@
-{lib, ...}:
+{ lib, ... }: let mkVariable = type: defaultValue: lib.mkOption { type = type; default = defaultValue; }; in
 {
-  options.variables = lib.mkOption { 
-    type = lib.types.attrsOf lib.types.anything; 
-    default = {
-      secrets = "/etc/nixos/secrets";
-      user = {
-        admin = "alexanderschiffhauer";
-        hashedPasswordFile = "hashed_password.txt";
-      };
-      disk = {
-        device = "OVERRIDE_THIS_VALUE_IN_HOST";
-        swapSize = "OVERRIDE_THIS_VALUE_IN_HOST";
-        tmpPasswordFile = "/tmp/plain_text_password.txt";
-        pkiBundle = "/var/lib/sbctl";
-      };
-    };
+  options = with lib.types; {
+    variables.secrets = mkVariable str "/etc/nixos/secrets";
+    variables.user.admin = mkVariable str "alexanderschiffhauer";
+    variables.user.hashedPasswordFile = mkVariable str "hashed_password.txt";
+    variables.disk.device = mkVariable str "OVERRIDE_THIS_VALUE_IN_HOST";
+    variables.disk.swapSize = mkVariable str "OVERRIDE_THIS_VALUE_IN_HOST";
+    variables.disk.tmpPasswordFile = mkVariable str "/tmp/plain_text_password.txt";
+    variables.disk.pkiBundle = mkVariable str "/var/lib/sbctl";
   };
 }
