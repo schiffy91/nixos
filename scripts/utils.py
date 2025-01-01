0fe5c9b997c11ff6416a56db313ec5f268c0fe9a
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-01 12:18:08 -0800

    Updates to MB-M1-VM

--- scripts/utils.py
+++ scripts/utils.py
@@ -190,8 +190,8 @@ class Interactive:
     @classmethod
     def ask_for_host_path(cls):
         hosts_paths = glob.glob(f"{Config.get_nixos_path()}/hosts/**/*.nix", recursive=True)
-        formatted_hosts_paths = [ cls.sh.basename(host_path).replace(".nix", "") for host_path in hosts_paths ]
-        potential_matches = [ formatted_hosts_path for formatted_hosts_path in formatted_hosts_paths if formatted_hosts_path == cls.sh.hostname() ]
+        formatted_hosts_paths = [ f"({cls.sh.parent_name(host_path)})" + cls.sh.basename(host_path).replace(".nix", "") for host_path in hosts_paths ]
+        potential_matches = [ formatted_hosts_path for formatted_hosts_path in formatted_hosts_paths if cls.sh.hostname() in formatted_hosts_path ]
         if potential_matches:
             match = hosts_paths[formatted_hosts_paths.index(potential_matches[0])]
             if Interactive.confirm(f"Use {match}?"): return match
