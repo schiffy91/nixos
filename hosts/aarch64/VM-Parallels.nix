35e05d6d304c5ec03bdf0f129aca05583f0c8448
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-02 08:09:32 -0800

    Updates

--- hosts/aarch64/VM-Parallels.nix
+++ hosts/aarch64/VM-Parallels.nix
@@ -15,27 +15,7 @@ let kernelPackage = "linux_6_11"; in # Parallels Tools is broken on anything gre
   ##### Boot Configuration #####
   boot.initrd.availableKernelModules = [
     "xhci_pci"
-    "virtio_pci"
-    "virtio_pci_modern_dev"
-    "drm"
-    "drm_kms_helper"
-    "virtio_mmio"
-    "virtio_blk"
-    "virtio_net"
-    "hid_generic"
-    "usbhid"
-    "9p"
-    "9pnet_virtio"
-    "snd_hda_codec"
-    "snd_hda_core"
-    "prl_fs_freeze"
-    "prl_fs"
-    "prl_tg"
-  ];
-  boot.initrd.kernelModules = [
-    "virtio_gpu"
-    "virtio_balloon"
-    "virtio_rng"
+    "srm_mod"
   ];
   ##### Packages #####
   environment.systemPackages = with pkgs; [
