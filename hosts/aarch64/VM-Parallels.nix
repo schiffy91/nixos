4165546fa3661ee3f941d57de18da7d1b8b34adb
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-02 08:29:52 -0800

    Updates

--- hosts/aarch64/VM-Parallels.nix
+++ hosts/aarch64/VM-Parallels.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 let
-  kernel_version = "6_6";
+  kernel_version = "6_6"; # Parallels Tools is broken on anything greater than 6.11. SMH
   kernelPackage = "linux_${kernel_version}";
   linuxPackage = "linuxPackages_${kernel_version}";
-in # Parallels Tools is broken on anything greater than 6.11. SMH
+in
 {
   ##### Host Name #####
   networking.hostName = "VM";
@@ -15,29 +15,12 @@ in # Parallels Tools is broken on anything greater than 6.11. SMH
     enable = true;
     package = pkgs.linuxKernel.packages.${kernelPackage}.prl-tools;
   };
-  ##### X11 Configuration #####
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      sddm.wayland.enable = false;  # Force X11
-    };
-  };
   boot.kernelPackages = pkgs.${linuxPackage};
   ##### Boot Configuration #####
   boot.initrd.availableKernelModules = [
     "xhci_pci"
     "sr_mod"
   ];
-  ##### Clipboard Sharing #####
-  systemd.user.services.prl-clipboard = {
-    description = "Parallels Tools Clipboard Service";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.linuxKernel.packages.${kernelPackage}.prl-tools}/bin/prlcc";
-      Restart = "always";
-    };
-  };
   ##### Packages #####
   environment.systemPackages = with pkgs; [
       linuxKernel.packages.${kernelPackage}.prl-tools
