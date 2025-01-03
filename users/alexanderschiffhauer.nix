7bcd1194bfd2583d7caaa8da68d917d1f95daa21
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 12:05:00 -0800

    Updates

--- users/alexanderschiffhauer.nix
+++ users/alexanderschiffhauer.nix
@@ -1,15 +1,3 @@
 { config, pkgs, ... }: {
   home.stateVersion = "24.11";
-  programs.plasma = {
-    enable = true;
-    workspace = {
-      lookAndFeel = "org.kde.breezedark.desktop";
-      cursor = {
-        theme = "Bibata-Modern-Ice";
-        size = 32 * config.variables.desktop.scalingFactor;
-      };
-      iconTheme = "Papirus-Dark";
-      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
-    };
-  };
 }
