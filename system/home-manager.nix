a8428cd4bdaf2e030c39caa2c65c811b2cfcc980
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 11:57:32 -0800

    Updates

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -1,7 +1,7 @@
-{ inputs, lib, ...}: {
+{ inputs, config, pkgs, lib, ...}: {
   imports = [inputs.home-manager.nixosModules.home-manager];
   home-manager = {
-    extraSpecialArgs = { inherit inputs ; };
+    extraSpecialArgs = { inherit inputs config pkgs lib; };
     useGlobalPkgs = true;
     useUserPackages = true;
     sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
