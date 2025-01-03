c68d5ab98190eea1a0837584b7a98c2b647f52f7
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 12:07:26 -0800

    Updates

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -1,4 +1,13 @@
 { inputs, config, pkgs, lib, ...}: {
   imports = [inputs.home-manager.nixosModules.home-manager];
- 
+  home-manager = {
+    extraSpecialArgs = { inherit inputs config pkgs lib; };
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
+    users = lib.listToAttrs (lib.map (path: { 
+      name = lib.removeSuffix ".nix" (baseNameOf path); 
+      value = import path; 
+    }) (lib.filter (path: lib.hasSuffix ".nix" path) (lib.filesystem.listFilesRecursive ../users)));
+  };
 }
