374ad67d454a20db0b31562928fcebbec0514e61
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 15:43:49 -0800

    Updates

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 { lib, ...}:
 {
-  imports = lib.filter (path: lib.hasSuffix ".nix" path) (lib.filesystem.listFilesRecursive ./modules/system);
+  imports = lib.filter (path: lib.hasSuffix ".nix" path) (lib.filesystem.listFilesRecursive ./system/modules);
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "24.11";
 }
