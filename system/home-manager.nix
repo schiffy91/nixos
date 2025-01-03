e0ae605324c95e5236ce512968a014d6dde638c6
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 12:37:23 -0800

    Updates

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -5,13 +5,21 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     users = let
-      allFiles = builtins.trace "All files found: " (lib.filesystem.listFilesRecursive ../users);
-      nixFiles = builtins.trace "Filtered .nix files: " (lib.filter (path: lib.hasSuffix ".nix" path) allFiles);
-      userAttrs = lib.map (path: {
-        name = builtins.trace "Processing user: " (lib.removeSuffix ".nix" (baseNameOf path));
-        value = import path;
-      }) nixFiles;
+      allFiles = let files = lib.filesystem.listFilesRecursive ../users; 
+                 in builtins.trace "All files found: ${toString files}" files;
+                 
+      nixFiles = let filtered = lib.filter (path: lib.hasSuffix ".nix" path) allFiles;
+                 in builtins.trace "Nix files found: ${toString filtered}" filtered;
+                 
+      userAttrs = lib.map (path: 
+        let username = lib.removeSuffix ".nix" (baseNameOf path);
+        in {
+          name = builtins.trace "Processing user: ${username} from path: ${toString path}" username;
+          value = import path;
+        }
+      ) nixFiles;
     in
-      builtins.trace "Final user attributes: " (lib.listToAttrs userAttrs);
+      builtins.trace "Final user attributes: ${toString (map (x: x.name) userAttrs)}"
+      (lib.listToAttrs userAttrs);
   };
 }
