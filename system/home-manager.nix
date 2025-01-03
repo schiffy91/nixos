297b168e091c4bda17b4c5c4ee7f9bf412c8f566
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 12:41:15 -0800

    Updates

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -5,24 +5,31 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     users = let
-      allFiles = let files = lib.filesystem.listFilesRecursive ../users; 
-                 in builtins.trace "All files found: ${toString files}" files;
-                 
-      nixFiles = let filtered = lib.filter (path: lib.hasSuffix ".nix" path) allFiles;
-                 in builtins.trace "Nix files found: ${toString filtered}" filtered;
-                 
-userAttrs = lib.map (path: 
-  let 
-    username = lib.removeSuffix ".nix" (baseNameOf path);
-    imported = 
-      if builtins.pathExists path
-      then builtins.trace "Importing config for ${username}" (import path)
-      else builtins.throw "Config file not found: ${toString path}";
-  in {
-    name = username;
-    value = builtins.trace "Loaded config for ${username}: ${toString (builtins.attrNames imported)}" imported;
-  }
-) nixFiles;
+      # List all files in users directory
+      allFiles = let 
+        files = lib.filesystem.listFilesRecursive ../users; 
+      in 
+        builtins.trace "All files found: ${toString files}" files;
+      
+      # Filter for .nix files
+      nixFiles = let 
+        filtered = lib.filter (path: lib.hasSuffix ".nix" path) allFiles;
+      in 
+        builtins.trace "Nix files found: ${toString filtered}" filtered;
+      
+      # Map files to user configurations
+      userAttrs = lib.map (path: 
+        let 
+          username = lib.removeSuffix ".nix" (baseNameOf path);
+          imported = 
+            if builtins.pathExists path
+            then builtins.trace "Importing config for ${username}" (import path { inherit inputs config pkgs lib; })
+            else builtins.throw "Config file not found: ${toString path}";
+        in {
+          name = username;
+          value = builtins.trace "Loaded config for ${username}" imported;
+        }
+      ) nixFiles;
     in
       builtins.trace "Final user attributes: ${toString (map (x: x.name) userAttrs)}"
       (lib.listToAttrs userAttrs);
