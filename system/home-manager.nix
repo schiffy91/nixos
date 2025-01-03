b9bb08c21297fcdf04f58b2f2a95486a343b4075
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 12:46:35 -0800

    Updates

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -4,36 +4,9 @@
     extraSpecialArgs = { inherit inputs config pkgs lib; };
     useGlobalPkgs = true;
     useUserPackages = true;
-    users = let
-      # Use absolute path resolution
-      usersPath = ../../users;
-      
-      # List all files in users directory
-      allFiles = let 
-        files = lib.filesystem.listFilesRecursive usersPath;
-      in 
-        builtins.trace "All files found: ${toString files}" files;
-      
-      # Filter for .nix files
-      nixFiles = let 
-        filtered = lib.filter (path: lib.hasSuffix ".nix" path) allFiles;
-      in 
-        builtins.trace "Nix files found: ${toString filtered}" filtered;
-      
-      # Map files to user configurations
-      userAttrs = lib.map (path: 
-        let 
-          username = lib.removeSuffix ".nix" (baseNameOf path);
-          imported = 
-            if builtins.pathExists path
-            then builtins.trace "Importing config for ${username}" (import path { inherit inputs config pkgs lib; })
-            else builtins.throw "Config file not found: ${toString path}";
-        in {
-          name = username;
-          value = imported;
-        }
-      ) nixFiles;
-    in
-      lib.listToAttrs userAttrs;
+    users = lib.listToAttrs (lib.map (path: { 
+      name = lib.removeSuffix ".nix" (baseNameOf path); 
+      value = import path; 
+    }) (lib.filter (path: lib.hasSuffix ".nix" path) (lib.filesystem.listFilesRecursive ../users)));
   };
 }
