837a6d1078f6f6fcb28c3489cd51fd8cfd3d94db
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 12:15:09 -0800

    Updates

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -5,6 +5,9 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
-
+    users = lib.listToAttrs (lib.map (path: { 
+      name = lib.removeSuffix ".nix" (baseNameOf path); 
+      value = import path; 
+    }) (lib.filter (path: lib.hasSuffix ".nix" path) (lib.filesystem.listFilesRecursive ../users)));
   };
 }
--- users/alexanderschiffhauer.nix
+++
@@ -1,3 +0,0 @@
-{ config, pkgs, ... }: {
-  home.stateVersion = "24.11";
-}
