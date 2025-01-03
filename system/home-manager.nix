007190a173b7dd76419ce2d2ae363eaee23927b6
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 12:36:33 -0800

    Updates

--- system/home-manager.nix
+++ system/home-manager.nix
@@ -4,9 +4,14 @@
     extraSpecialArgs = { inherit inputs config pkgs lib; };
     useGlobalPkgs = true;
     useUserPackages = true;
-    users = lib.listToAttrs (lib.map (path: { 
-      name = lib.removeSuffix ".nix" (baseNameOf path); 
-      value = import path; 
-    }) (lib.filter (path: lib.hasSuffix ".nix" path) (lib.filesystem.listFilesRecursive ../users)));
+    users = let
+      allFiles = builtins.trace "All files found: " (lib.filesystem.listFilesRecursive ../users);
+      nixFiles = builtins.trace "Filtered .nix files: " (lib.filter (path: lib.hasSuffix ".nix" path) allFiles);
+      userAttrs = lib.map (path: {
+        name = builtins.trace "Processing user: " (lib.removeSuffix ".nix" (baseNameOf path));
+        value = import path;
+      }) nixFiles;
+    in
+      builtins.trace "Final user attributes: " (lib.listToAttrs userAttrs);
   };
 }
