f964d5afbc78a17a811765e523304dd9c2474f80
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 16:21:25 -0800

    Updates

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-24.11"; };
     nixpkgs-unstable = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     disko = { url = "github:nix-community/disko/v1.10.0"; inputs.nixpkgs.follows = "nixpkgs"; };
-    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    home-manager = { url = "github:nix-community/home-manager/release-24.11"; inputs.nixpkgs.follows = "nixpkgs"; };
     sbctl-pkg = { url = "github:NixOS/nixpkgs/93dc9803a1ee435e590b02cde9589038d5cc3a4e"; }; #TODO nixpkgs currently uses v0.14; update to v0.16 after the package is updated in nixpkgs.
     lanzaboote = { url = "github:nix-community/lanzaboote/93e6f0d77548be8757c11ebda5c4235ef4f3bc67"; inputs.nixpkgs.follows = "nixpkgs"; }; #TODO Lanzaboote is not in nixpgs, and it's latest release version uses sbctl v0.14; update to a stable release after its pushed to github.
   };
