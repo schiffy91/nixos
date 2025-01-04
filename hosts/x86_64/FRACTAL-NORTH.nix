50b45247eda3021ebb48ec8ad477d76cc1c8fa01
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 18:52:34 -0800

    Trying a simpler desktop config

--- hosts/x86_64/FRACTAL-NORTH.nix
+++ hosts/x86_64/FRACTAL-NORTH.nix
@@ -6,59 +6,7 @@
   variables.disk.device = "/dev/nvme0n1"; # This line must exist, but feel free to change the location
   variables.disk.swapSize = "65G";
   # Dual GPU drivers
-  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
   # AMD drivers
   boot.kernelParams = [ "amdgpu.dc=1" ];
-  #hardware.firmware = [ pkgs.amd-ucode ];
-  # Nvidia drivers
-  hardware.nvidia = {
-    open = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    nvidiaSettings = true;
-    powerManagement.enable = true;
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      amdgpuBusId = "PCI:1:0:0";
-      nvidiaBusId = "PCI:69:0:0";
-    };
-  };
-  # Virtualization (libvirt, podman)
-  services.qemuGuest.enable = true;
-  programs.virt-manager.enable = true;
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
-        swtpm.enable = true;
-        runAsRoot = false;
-      };
-    };
-    spiceUSBRedirection.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-    };
-  };
-
-  # Packages
-  environment.systemPackages = with pkgs; [
-    google-chrome
-    distrobox
-    vscode
-    pciutils
-    usbutils
-    virt-manager
-    virt-viewer
-    spice 
-    spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
-  ];
 }
