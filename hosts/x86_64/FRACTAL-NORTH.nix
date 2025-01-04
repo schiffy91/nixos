c5a9e1154103058e020e30ef408341376b6f8941
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 18:45:14 -0800

    Trying a simpler desktop config

--- hosts/x86_64/FRACTAL-NORTH.nix
+++ hosts/x86_64/FRACTAL-NORTH.nix
@@ -1,30 +1,64 @@
 { config, pkgs, ... }:
-{
+{  
   ##### Host Name #####
   networking.hostName = "FRACTAL-NORTH";
-
-  # Basic display configuration
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "amdgpu" ];
+  # Disk information
+  variables.disk.device = "/dev/nvme0n1"; # This line must exist, but feel free to change the location
+  variables.disk.swapSize = "65G";
+  # Dual GPU drivers
+  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+  # AMD drivers
+  boot.kernelParams = [ "amdgpu.dc=1" ];
+  hardware.firmware = [ pkgs.amd-ucode ];
+  # Nvidia drivers
+  hardware.nvidia = {
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidiaSettings = true;
+    powerManagement.enable = true;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      amdgpuBusId = "PCI:1:0:0";
+      nvidiaBusId = "PCI:69:0:0";
+    };
+  };
+  # Virtualization (libvirt, podman)
+  services.qemuGuest.enable = true;
+  programs.virt-manager.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+        swtpm.enable = true;
+        runAsRoot = false;
+      };
+    };
+    spiceUSBRedirection.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+    };
   };
 
-  # AMD GPU support
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      amdvlk
-      rocm-opencl-icd
-      rocm-opencl-runtime
-    ];
-  };
-
-  # Essential system packages
+  # Packages
   environment.systemPackages = with pkgs; [
-    pciutils    # For hardware inspection
-    glxinfo     # For GPU info
-    radeontop   # For AMD GPU monitoring
+    google-chrome
+    distrobox
+    vscode
+    pciutils
+    usbutils
+    virt-manager
+    virt-viewer
+    spice 
+    spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
   ];
 }
