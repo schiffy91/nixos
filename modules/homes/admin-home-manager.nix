03a7cda5b71d7cd96b3107f9e66865ffae98f680
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 17:02:57 -0800

    Updates

--- modules/homes/admin-home-manager.nix
+++ modules/homes/admin-home-manager.nix
@@ -1,4 +1,4 @@
-{ variables, ... }: {
+{ variables, pkgs, ... }: {
   home = {
     username = variables.user.admin;
     homeDirectory = "/home/${variables.user.admin}";
@@ -10,4 +10,32 @@
       [[ssh-keys]]
       vault = "Private"
     '';
+  programs.plasma = {
+    enable = true;
+    workspace = {
+      lookAndFeel = "org.kde.breezedark.desktop";
+      cursor.theme = "Bibata-Modern-Ice";
+      iconTheme = "Papirus-Dark";
+      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
+    };
+
+    panels = [
+      {
+        location = "bottom";
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.icontasks"
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
+      }
+      # Global menu at the top
+      {
+        location = "top";
+        height = 26;
+        widgets = [ "org.kde.plasma.appmenu" ];
+      }
+    ];
+  };
 }
