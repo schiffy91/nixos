b8f7ecad74537e94c8ea5ed33a8e109cf20ec764
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 17:47:05 -0800

    Updates

--- modules/homes/admin-home-manager.nix
+++ modules/homes/admin-home-manager.nix
@@ -1,4 +1,4 @@
-{ variables, pkgs, lib, ... }: {
+{ variables, pkgs, ... }: {
   home = {
     username = variables.user.admin;
     homeDirectory = "/home/${variables.user.admin}";
@@ -7,31 +7,96 @@
   programs.home-manager.enable = true;
   # Configure 1Password SSH Agent to add all my SSH Keys
   xdg.configFile."1Password/ssh/agent.toml".text = ''
-      [[ssh-keys]]
-      vault = "Private"
-    '';
+    [[ssh-keys]]
+    vault = "Private"
+  '';
+  # Declarative Plasma :)
   programs.plasma = {
     enable = true;
     workspace = {
       lookAndFeel = "org.kde.breezedark.desktop";
       cursor.theme = "Bibata-Modern-Ice";
-      iconTheme = "Papirus-Dark";
-      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
+      iconTheme = "Papirus-Light";
+      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Next/contents/images_dark/5120x2880.png";
     };
-
-    panels = [{
-      location = "bottom";
-      widgets = [
-        "org.kde.plasma.kickoff"
-        "org.kde.plasma.icontasks"
-        "org.kde.plasma.marginsseparator"
-        "org.kde.plasma.systemtray"
-        "org.kde.plasma.digitalclock"];
+    panels = [
+      {
+        location = "bottom";
+        widgets = [
+          {
+            kickoff = {
+              sortAlphabetically = true;
+              icon = "nix-snowflake-white";
+            };
+          }
+          {
+            iconTasks = {
+              launchers = [
+                "applications:org.kde.dolphin.desktop"
+                "applications:org.kde.konsole.desktop"
+              ];
+            };
+          }
+          "org.kde.plasma.marginsseparator"
+          {
+            digitalClock = {
+              calendar.firstDayOfWeek = "sunday";
+              time.format = "12h";
+            };
+          }
+          {
+            systemTray.items = {
+              shown = [
+                "org.kde.plasma.networkmanagement"
+              ];
+              hidden = [
+                "org.kde.plasma.volume"
+                "org.kde.plasma.battery"
+                "org.kde.plasma.bluetooth"
+                "org.kde.plasma.clipboard"
+              ];
+            };
+          }
+        ];
+        hiding = "autohide";
       }
     ];
+
+    window-rules = [
+      {
+        description = "Dolphin";
+        match = {
+          window-class = {
+            value = "dolphin";
+            type = "substring";
+          };
+          window-types = [ "normal" ];
+        };
+        apply = {
+          noborder = {
+            value = true;
+            apply = "force";
+          };
+          maximizehoriz = true;
+          maximizevert = true;
+        };
+      }
+    ];
+
+    kwin = {
+      edgeBarrier = 0;
+      cornerBarrier = false;
+      scripts.polonium.enable = true;
+    };
+
+    kscreenlocker = {
+      lockOnResume = true;
+      timeout = 10;
+    };
+
     configFile = {
       "kdeglobals"."KScreen"."ScaleFactor" = 1 * variables.desktop.scalingFactor;
-      "kdeglobals"."KScreen"."ScreenScaleFactors" = "Virtual-1=${builtins.toString (1 * variables.desktop.scalingFactor)};";
+      "kdeglobals"."KScreen"."ScreenScaleFactors" = "Virtual-1=${toString (1 * variables.desktop.scalingFactor)};";
     };
   };
 }
