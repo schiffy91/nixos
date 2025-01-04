a536c2e61195be1cc505965f2d2a32f146ffaeaf
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 22:29:38 -0800

    Security fixes

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -3,23 +3,26 @@ let
   lanSubnet = "10.0.0.0/24";
   sshPort = 22;
   mdnsPort = 5353; # Avahi
-  sunshinePort = 47984; # Replace with the actual Sunshine port
+  sunshineTcpPorts = [ 47984 47989 47990 48010 ];
+  sunshineUdpPortRanges = [ { from = 47998; to = 48000; } { from = 8000; to = 8010; } ];
 in
  {
   # Allow SSH, MDNS, and Sunshine only over LAN
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = []; 
+    allowedTCPPorts = [];
     allowedUDPPorts = [];
     extraCommands = ''
       iptables -A nixos-fw -p tcp --dport ${toString sshPort} -s ${lanSubnet} -j nixos-fw-accept
       iptables -A nixos-fw -p udp --dport ${toString mdnsPort} -s ${lanSubnet} -j nixos-fw-accept
-      iptables -A nixos-fw -p tcp --dport ${toString sunshinePort} -s ${lanSubnet} -j nixos-fw-accept
+      ${lib.concatStringsSep "\n" (map (port: ''iptables -A nixos-fw -p tcp --dport ${toString port} -s ${lanSubnet} -j nixos-fw-accept'') sunshineTcpPorts)}
+      ${lib.concatStringsSep "\n" (map (range: ''iptables -A nixos-fw -p udp --dport ${toString range.from}:${toString range.to} -s ${lanSubnet} -j nixos-fw-accept'') sunshineUdpPortRanges)}
     '';
     extraStopCommands = ''
       iptables -D nixos-fw -p tcp --dport ${toString sshPort} -s ${lanSubnet} -j nixos-fw-accept || true
       iptables -D nixos-fw -p udp --dport ${toString mdnsPort} -s ${lanSubnet} -j nixos-fw-accept || true
-      iptables -D nixos-fw -p tcp --dport ${toString sunshinePort} -s ${lanSubnet} -j nixos-fw-accept || true
+      ${lib.concatStringsSep "\n" (map (port: ''iptables -D nixos-fw -p tcp --dport ${toString port} -s ${lanSubnet} -j nixos-fw-accept || true'') sunshineTcpPorts)}
+      ${lib.concatStringsSep "\n" (map (range: ''iptables -D nixos-fw -p udp --dport ${toString range.from}:${toString range.to} -s ${lanSubnet} -j nixos-fw-accept || true'') sunshineUdpPortRanges)}
     '';
   };
   services = {
