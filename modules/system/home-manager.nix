37193fb00cbb473ae1dfc8af4795fef07e850fb1
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 17:58:01 -0800

    Updates

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -5,6 +5,6 @@
     useUserPackages = true;
     sharedModules = [ inputs.plasma-manager.homeManagerModules.plasma-manager ];
     extraSpecialArgs = { variables = config.variables; };
-    users."${config.variables.user.admin}" = import ../homes/admin-home-manager.nix;
+    users."${config.variables.user.admin}" = import ../user/home.nix;
   };
 }
