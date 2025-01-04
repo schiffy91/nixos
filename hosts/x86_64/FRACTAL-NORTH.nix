c44b4d74d1ae8362f55d4a130eb4fe40430bd73e
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 18:33:50 -0800

    Trying a simpler desktop config

--- hosts/x86_64/FRACTAL-NORTH.nix
+++ hosts/x86_64/FRACTAL-NORTH.nix
@@ -1,61 +1,30 @@
 { config, pkgs, ... }:
-{  
+{
   ##### Host Name #####
   networking.hostName = "FRACTAL-NORTH";
-  # Disk information
-  variables.disk.device = "/dev/nvme0n1"; # This line must exist, but feel free to change the location
-  variables.disk.swapSize = "65G";
-  # Nvidia drivers
-  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
-  hardware.nvidia = {
-    open = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    nvidiaSettings = true;
-    powerManagement.enable = true;
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      amdgpuBusId = "PCI:1:0:0";
-      nvidiaBusId = "PCI:69:0:0";
-    };
+
+  # Basic display configuration
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "amdgpu" ];
   };
 
-  # Virtualization (libvirt, podman)
-  services.qemuGuest.enable = true;
-  programs.virt-manager.enable = true;
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
-        swtpm.enable = true;
-        runAsRoot = false;
-      };
-    };
-    spiceUSBRedirection.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-    };
+  # AMD GPU support
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      amdvlk
+      rocm-opencl-icd
+      rocm-opencl-runtime
+    ];
   };
 
-  # Packages
+  # Essential system packages
   environment.systemPackages = with pkgs; [
-    google-chrome
-    distrobox
-    vscode
-    pciutils
-    usbutils
-    virt-manager
-    virt-viewer
-    spice 
-    spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
+    pciutils    # For hardware inspection
+    glxinfo     # For GPU info
+    radeontop   # For AMD GPU monitoring
   ];
 }
