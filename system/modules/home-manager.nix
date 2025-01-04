e7c09fa3f458dccd73a06d3a864563778571e11f
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 16:08:45 -0800

    Updates

--- system/modules/home-manager.nix
+++ system/modules/home-manager.nix
@@ -1,4 +1,4 @@
-{ inputs, config, ... }: inputs.home-manager.nixosModules.home-manager {
+{ inputs, config, lib, ... }: inputs.home-manager.nixosModules.home-manager lib {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users."${config.variables.user.admin}" = import ../users/admin.nix;
