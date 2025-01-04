4e83729ccb1204292c4593fcebf06148ee612577
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-03 16:34:49 -0800

    Updates

--- system/modules/home-manager.nix
+++ system/modules/home-manager.nix
@@ -11,11 +11,11 @@
         stateVersion = "24.11";
       };
       programs.home-manager.enable = true;
+      # Configure 1Password SSH Agent to add all my SSH Keys
+      xdg.configFile."1Password/ssh/agent.toml".text = ''
+          [[ssh-keys]]
+          vault = "Private"
+        '';
     };
-    # Configure 1Password SSH Agent to add all my SSH Keys
-    xdg.configFile."1Password/ssh/agent.toml".text = ''
-        [[ssh-keys]]
-        vault = "Private"
-      '';
   };
 }
