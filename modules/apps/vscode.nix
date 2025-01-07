1fba6de4b047172ef6830227d589b4958054824d
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-07 08:38:10 -0800

    Moving more apps to 1Password

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -3,7 +3,6 @@
   home.packages = [
     unstable-pkgs.vscode
     unstable-pkgs.vscode.fhs
-    pkgs.nx-ld
   ];
   #programs.nix-ld.enable = true; # See https://nixos.wiki/wiki/Visual_Studio_Code#Remote_SSH 
 }
