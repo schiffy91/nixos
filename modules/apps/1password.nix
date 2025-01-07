c248039564607de4d6e58eaad1671a33fb678a2c
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-07 08:37:04 -0800

    Moving more apps to 1Password

--- modules/apps/1password.nix
+++ modules/apps/1password.nix
@@ -3,11 +3,6 @@
     pkgs._1password-gui
     pkgs._1password-cli
   ];
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    polkitPolicyOwners = [ "${settings.user.admin.username}" ];
-  };
   xdg.configFile."1Password/ssh/agent.toml".text = ''
     [[ssh-keys]]
     vault = "Private"
