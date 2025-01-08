ab986d597efcb930abd57b4cb3388eb62fd6a96c
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-07 22:30:47 -0800

    Making disk encryption optional for the VM

--- modules/system/disk.nix
+++ modules/system/disk.nix
@@ -1,10 +1,7 @@
 { config, inputs, ... }:
 let 
   mkSwapVolume = swapSize: if swapSize == "" then {} else {
-    "/swap" = { 
-      mountpoint = "/.swapvol"; 
-      swap.swapfile.size = swapSize; 
-    };
+
   };
   mkRootVolume = diskEncryption: content: 
     if diskEncryption then {
@@ -24,46 +21,48 @@ let
     };
 in {
   imports = [ inputs.disko.nixosModules.disko ];
-  disko.devices.disk = {
-    "${config.settings.disk.label.nixos}" = {
-      type = "disk";
-      device = config.settings.disk.device;
-      content = {
-        type = "gpt";
-        partitions = {
-          "${config.settings.disk.label.boot}" = {
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
-            };
-          } // 
-          mkRootVolume config.settings.disk.encryption.enabled {
-            type = "btrfs";
-            extraArgs = [ "-f" ];
-            subvolumes = {
-              "/root" = {
-                mountpoint = "/";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
-              "/home" = {
-                mountpoint = "/home";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
-              "/nix" = {
-                mountpoint = "/nix";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
-              "/var" = {
-                mountpoint = "/var";
-                mountOptions = [ "compress=zstd" "noatime" ];
-              };
-            } // (mkSwapVolume config.settings.disk.swapSize);
+  disko.devices.disk."${config.settings.disk.label.nixos}" = {
+    type = "disk";
+    device = config.settings.disk.device;
+    content = {
+      type = "gpt";
+      partitions = {
+        "${config.settings.disk.label.boot}" = {
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
           };
         };
+      } // mkRootVolume config.settings.disk.encryption.enabled {
+        type = "btrfs";
+        extraArgs = [ "-f" ];
+        subvolumes = {
+          "/root" = {
+            mountpoint = "/";
+            mountOptions = [ "compress=zstd" "noatime" ];
+          };
+          "/home" = {
+            mountpoint = "/home";
+            mountOptions = [ "compress=zstd" "noatime" ];
+          };
+          "/nix" = {
+            mountpoint = "/nix";
+            mountOptions = [ "compress=zstd" "noatime" ];
+          };
+          "/var" = {
+            mountpoint = "/var";
+            mountOptions = [ "compress=zstd" "noatime" ];
+          };
+        } // (if mkSwapVolume config.settings.disk.swapSize == "" then { } else {
+          "/swap" = { 
+            mountpoint = "/.swapvol"; 
+            swap.swapfile.size = mkSwapVolume config.settings.disk.swapSize; 
+          };
+        });
       };
     };
   };
