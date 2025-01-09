c2e95959aee938c10be226c86baede79cb4e8e59
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-08 23:07:49 -0800

    Update BlackBox settings: adjust cursor shape, font size, and theme; add new Catppuccin-Frappe color scheme

--- modules/apps/blackbox.nix
+++ modules/apps/blackbox.nix
@@ -3,15 +3,15 @@
   dconf.settings = {
     "com/raggesilver/BlackBox" = {
       cursor-blink-mode = lib.hm.gvariant.mkUint32 1;
-      cursor-shape = lib.hm.gvariant.mkUint32 0;
+      cursor-shape = lib.hm.gvariant.mkUint32 1;
       easy-copy-paste = true;
       floating-controls = true;
       floating-controls-hover-area = lib.hm.gvariant.mkUint32 20;
-      font = "Hack 16";
+      font = "Hack 11";
       pretty = true;
       remember-window-size = true;
       scrollback-lines = lib.hm.gvariant.mkUint32 10240;
-      theme-dark = "Catppuccin-Mocha";
+      theme-dark = "Catppuccin-Frappe";
       window-height = lib.hm.gvariant.mkUint32 1150;
       window-width = lib.hm.gvariant.mkUint32 1450;
     };
@@ -55,4 +55,41 @@
       }
     '';
   };
+  ".local/share/blackbox/schemes/Catppuccin-Frappe.json".text = ''
+    {
+      "name": "Catppuccin Frappé",
+      "comment": "Soothing pastel theme for Tilix",
+      "background-color": "#303446",
+      "foreground-color": "#c6d0f5",
+      "badge-color": "#626880",
+      "bold-color": "#626880",
+      "cursor-background-color": "#f2d5cf",
+      "cursor-foreground-color": "#303446",
+      "highlight-background-color": "#f2d5cf",
+      "highlight-foreground-color": "#303446",
+      "palette": [
+        "#b5bfe2",
+        "#e78284",
+        "#a6d189",
+        "#e5c890",
+        "#8caaee",
+        "#f4b8e4",
+        "#81c8be",
+        "#626880",
+        "#a5adce",
+        "#e78284",
+        "#a6d189",
+        "#e5c890",
+        "#8caaee",
+        "#f4b8e4",
+        "#81c8be",
+        "#51576d"
+      ],
+      "use-badge-color": false,
+      "use-bold-color": false,
+      "use-cursor-color": true,
+      "use-highlight-color": true,
+      "use-theme-colors": false
+    }
+  '';
 }
