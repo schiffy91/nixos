94ed58f168f09dcb67c1d97758473e3e39b445e4
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-09 22:50:12 -0800

    Refactor plasma.nix: remove unused window decorations and kwin effects settings

--- modules/apps/plasma.nix
+++ modules/apps/plasma.nix
@@ -1,9 +1,5 @@
 { settings, pkgs, ... }: {
   home.packages = with pkgs; [ papirus-icon-theme ];
-  xdg.dataFile."aurorae/themes/ActiveAccentDark".source = "${pkgs.fetchzip { 
-    url = "https://github.com/nclarius/Plasma-window-decorations/archive/02058699173f5651816d4cb31960d08b45553255.zip"; 
-    sha256 = "sha256-O4JTtj/q2lJRhWS+nhfQes8jitkrfsSBmENHZb5ioNI=";
-  }}/ActiveAccentDark";
   programs.plasma = {
     enable = true;
     workspace = {
@@ -11,8 +7,6 @@
       cursor.theme = "Bibata-Modern-Ice";
       wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Next/contents/images_dark/5120x2880.png";
       iconTheme = "Papirus-Dark";
-      windowDecorations.theme = "ActiveAccentDark";
-      windowDecorations.library = "org.kde.kwin.aurorae";
       cursor.size = builtins.floor (24 * settings.desktop.scalingFactor);
     };
     panels = [
@@ -68,24 +62,5 @@
       "kdeglobals"."KScreen"."ScreenScaleFactors" = "Virtual-1=${toString (1 * settings.desktop.scalingFactor)};";
       "kwinrc"."Xwayland"."Scale" = 1 * settings.desktop.scalingFactor;
     };
-    kwin.effects = {
-      blur.enable = true;
-      blur.noiseStrength = 10;
-      blur.strength = 5;
-      desktopSwitching.animation = "slide";
-      dimInactive.enable = false;
-      minimization.animation = "off";
-      shakeCursor.enable = true;
-      translucency.enable = true;
-    };
-    kwin.titlebarButtons = {
-      left = [ ];
-      right = [
-        "minimize"
-        "maximize"
-        "close"
-      ];
-    };
-    windows.allowWindowsToRememberPositions = false;
   };
 }
