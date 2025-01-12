93a5a44937050d053d871c044f59a5913d26f8b8
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-12 14:53:25 -0800

    Enable JSON output formatting in eval.py by setting json_format argument to True

--- bin/eval.py
+++ bin/eval.py
@@ -5,7 +5,7 @@ from nixos import Utils, Config
 
 def main():
     if len(sys.argv[1:]) != 1: return Utils.abort("Usage: eval.py attribute\nExample: eval.py config.settings.disk.device")
-    try: Utils.print(Config.eval(sys.argv[1:][0]))
+    try: Utils.print(Config.eval(sys.argv[1:][0], json_format=True))
     except BaseException as exception:
         Utils.log_error(f"Caught exception: {exception}.")
         raise
