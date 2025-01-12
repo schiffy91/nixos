fa9d794219f67331bbf8d53e7ef994e8d465e4d6
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-12 14:54:58 -0800

    Remove JSON formatting support from eval.py and nixos.py

--- bin/eval.py
+++ bin/eval.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i python3 -p python3
-import sys, json
+import sys
 from nixos import Utils, Config
 
 def main():
     if len(sys.argv[1:]) != 1: return Utils.abort("Usage: eval.py attribute\nExample: eval.py config.settings.disk.device")
-    try: Utils.print(Config.eval(sys.argv[1:][0], json_format=True))
+    try: Utils.print(Config.eval(sys.argv[1:][0]))
     except BaseException as exception:
         Utils.log_error(f"Caught exception: {exception}.")
         raise
