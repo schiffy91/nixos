a17389fef2f1ed9eed8bad92fc7b6721b8e460f8
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-12 11:57:35 -0800

    Enhance installer script to provide feedback on secrets reset

--- bin/install.py
+++ bin/install.py
@@ -50,8 +50,9 @@ def main():
     Installer.parse_args()
     if Installer.sh.exists(Config.get_config_path()): Utils.print(f"Found {Config.get_config_path()}")
     else: 
-        Utils.print(f"Not found")
+        Utils.print("Not found")
         Config.reset_config(Interactive.ask_for_host_path(), Config.get_standard_flake_target()) # Create config.json based on the selected host
+    Utils.print("Resetting secrets")
     Config.create_secrets(plain_text_password_path=Installer.get_plain_text_password_path()) # Create all secrets
     if Interactive.confirm(f"Format {Installer.get_installation_disk()}?"): Installer.erase_and_mount_disk() # Format disk
     else: Installer.mount_disk() # Or just mount disk
