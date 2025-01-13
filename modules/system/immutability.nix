cbd2ea134563cd36ecdb7906e8d89d9d001a94ad
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-12 22:35:23 -0800

    Comment out boot.readOnlyNixStore assignment in immutability configuration

--- modules/system/immutability.nix
+++ modules/system/immutability.nix
@@ -1,7 +1,7 @@
 { inputs, config, lib, pkgs, ... }: {
   imports = [ inputs.impermanence.nixosModules.impermanence ];
   fileSystems."${config.settings.disk.subvolumes.persist.mountPoint}".neededForBoot = true;
-  boot.readOnlyNixStore = config.settings.disk.immutability.enable;
+  #boot.readOnlyNixStore = config.settings.disk.immutability.enable;
   environment.persistence."${config.settings.disk.subvolumes.persist.mountPoint}" = {
     enable = config.settings.disk.immutability.enable;
     directories = config.settings.disk.immutability.persist.directories;
