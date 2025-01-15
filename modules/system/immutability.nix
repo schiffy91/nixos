1770abb1b7a72b415c0c77884ae76ba068b1965e
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-15 10:43:47 -0800

    Refactor immutability service setup to improve structure and enhance boot-time enforcement

--- modules/system/immutability.nix
+++ modules/system/immutability.nix
@@ -15,47 +15,61 @@ lib.mkIf config.settings.disk.immutability.enable {
           copy_bin_and_libs $pkg
         done
       '';
-    systemd.services.immutability = {
-      description = "Enforce immutability at boot-time by deleting all new paths that aren't managed by NixOS or marked to persist.";
-      wantedBy = [ "initrd.target" ];
-      before = [ "sysroot.mount" ];
-      after = if config.settings.disk.encryption.enable then [ "systemd-cryptsetup@*.service" ] else [ ];
-      path = initrdPkgs;
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig = {
-        Type = "oneshot";
-        StandardOutput = "journal+console";
-        StandardError = "journal+console";
+    systemd.services = {
+      setup-immutability = {
+        description = "Setup tools for immutability service";
+        wantedBy = [ "initrd.target" ];
+        before = [ "immutability.service" ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig = {
+          Type = "oneshot";
+          StandardOutput = "journal+console";
+          StandardError = "journal+console";
+          RemainAfterExit = "yes";
+        };
+        path = initrdPkgs;
+        script = ''
+          export PATH="${lib.makeBinPath initrdPkgs}:$PATH"
+          mkdir -p /usr/local/bin
+          cat > /usr/local/bin/immutability.sh << 'EOL'
+          ${(builtins.readFile immutabilityFilePath)}
+          EOL
+          chmod +x /usr/local/bin/immutability.sh
+        '';
       };
-      script = ''
-        # Ensure basic utils are available
-        export PATH="${lib.makeBinPath initrdPkgs}:$PATH"
-        
-        # Write the script to a file in the initrd
-        mkdir -p /usr/local/bin
-        cat > /usr/local/bin/immutability.sh << 'EOL'
-        ${(builtins.readFile immutabilityFilePath)}
-        EOL
-        chmod +x /usr/local/bin/immutability.sh
-        
-        # For each subvolume, mount and process
-        for i in ${config.settings.disk.subvolumes.volumesNeededForBoot}; do 
-          vol=$(echo "$i" | cut -d'=' -f1)  # Fix pattern matching
-          mount_point="/mnt/$vol"
-          mkdir -p "$mount_point"
-          
-          if mount -t btrfs -o subvol="$vol" "${config.settings.disk.by.partlabel.root}" "$mount_point"; then
-            /usr/local/bin/immutability.sh \
-              "$mount_point" \
-              "$mount_point/${config.settings.disk.immutability.persist.snapshots.name}" \
-              '${lib.strings.concatStringsSep " " config.settings.disk.immutability.persist.paths}'
-            umount "$mount_point"
-          else
-            echo "Failed to mount $vol"
-            exit 1
-          fi
-        done
-      '';
+      immutability = {
+        description = "Enforce immutability at boot-time";
+        wantedBy = [ "initrd.target" ];
+        after = [ "setup-immutability.service" ] ++ (if config.settings.disk.encryption.enable then [ "systemd-cryptsetup@*.service" ] else [ ]);
+        before = [ "sysroot.mount" ];
+        requires = [ "setup-immutability.service" ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig = {
+          Type = "oneshot";
+          StandardOutput = "journal+console";
+          StandardError = "journal+console";
+        };
+        path = initrdPkgs;
+        script = ''
+          export PATH="${lib.makeBinPath initrdPkgs}:$PATH"
+          for i in ${config.settings.disk.subvolumes.volumesNeededForBoot}; do 
+            vol=$(echo "$i" | cut -d'=' -f1)
+            mount_point="/mnt/$vol"
+            mkdir -p "$mount_point"
+            
+            if mount -t btrfs -o subvol="$vol" "${config.settings.disk.by.partlabel.root}" "$mount_point"; then
+              /usr/local/bin/immutability.sh \
+                "$mount_point" \
+                "$mount_point/${config.settings.disk.immutability.persist.snapshots.name}" \
+                '${lib.strings.concatStringsSep " " config.settings.disk.immutability.persist.paths}'
+              umount "$mount_point"
+            else
+              echo "Failed to mount $vol"
+              exit 1
+            fi
+          done
+        '';
+      };
     };
   };
 }
