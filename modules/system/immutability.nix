da9f7d8a10fb617af8b497c4cae548de36142fe7
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-15 09:53:09 -0800

    Update immutability script to create and execute /immutability.sh for boot volume management

--- modules/system/immutability.nix
+++ modules/system/immutability.nix
@@ -1,16 +1,14 @@
 { config, pkgs, lib, ... }:
-
 lib.mkIf config.settings.disk.immutability.enable {
-  ### This merges existing fileSystems or do whatever is needed if you want to
-  ### forcibly mark them as neededForBoot, read-only store, etc.
-  fileSystems = lib.mkMerge (map (mountPoint: {
-    "${mountPoint}".neededForBoot = true;
-  }) config.settings.disk.subvolumes.bootMountPoints);
-
+  fileSystems = lib.mkMerge (map (mountPoint: { "${mountPoint}".neededForBoot = lib.mkForce true; }) config.settings.disk.subvolumes.bootMountPoints);
   boot.readOnlyNixStore = true;
   boot.initrd.postResumeCommands = lib.mkAfter ''
+    cat << "EOF" > /immutability.sh
+    ${(builtins.readFile ../../bin/immutability.sh)}
+    EOF
+    chmod +x /immutability.sh
     for i in ${config.settings.disk.subvolumes.volumesNeededForBoot}; do vol=$i%=*; mount_point=/mnt/$vol;
-      mount -t btrfs -o subvol=$vol ${config.settings.disk.by.partlabel.root} $mount_point; /opt/restore-subvolume.sh $mount_point $mount_point/${config.settings.disk.immutability.persist.snapshots.name} '${lib.strings.concatStringsSep " " config.settings.disk.immutability.persist.paths}';
+      mount -t btrfs -o subvol=$vol ${config.settings.disk.by.partlabel.root} $mount_point; /immutability.sh $mount_point $mount_point/${config.settings.disk.immutability.persist.snapshots.name} '${lib.strings.concatStringsSep " " config.settings.disk.immutability.persist.paths}';
       umount $mount_point; done
   '';
 }
