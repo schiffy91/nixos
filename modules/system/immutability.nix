0d38e29d7f05d40c5043c9be82977664411f297f
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-16 10:00:28 -0800

    Refactor immutability.nix to improve pathsToKeep handling with shell argument escaping and update mount command to use subvolid for better compatibility

--- modules/system/immutability.nix
+++ modules/system/immutability.nix
@@ -5,7 +5,7 @@ let
   rootSubvolumeName = config.settings.disk.subvolumes.root.name;
   snapshotsSubvolumeName = config.settings.disk.subvolumes.snapshots.name;
   cleanRootSnapshotRelativePath = config.settings.disk.immutability.persist.snapshots.cleanRoot;
-  pathsToKeep = ''${lib.strings.concatStringsSep " " config.settings.disk.immutability.persist.paths}'';
+  pathsToKeep = lib.strings.concatStringsSep " " (map lib.strings.escapeShellArg config.settings.disk.immutability.persist.paths);
   rootDevice = "dev-disk-by\\x2dpartlabel-${config.settings.disk.label.disk}\\x2d${config.settings.disk.label.main}\\x2d${config.settings.disk.label.root}.device"; # JFC…
 in 
 lib.mkIf config.settings.disk.immutability.enable {
@@ -48,7 +48,7 @@ lib.mkIf config.settings.disk.immutability.enable {
           exit 1
       fi                       
       ##### Mount subvolumes #####
-      mount -t btrfs -o subvol=/,user_subvol_rm_allowed "$DEVICE" "$MOUNT"
+      mount -t btrfs -o subvolid=5,user_subvol_rm_allowed "$DEVICE" "$MOUNT"
       ##### Validate CLEAN_ROOT exists #####
       if [ -z "$CLEAN_ROOT" ] || [ ! -d "$CLEAN_ROOT" ]; then
         echo "Error: '$CLEAN_ROOT' is not a directory or is empty"
