9e7c81f152fb2c8a8401d0a448793c33e9a1ae83
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-22 09:42:15 -0700

    Refactor diff function to use btrfs send for snapshot comparison and update invocation of btrfs-snapshots-diff.py

--- bin/immutability.py
+++ bin/immutability.py
@@ -16,9 +16,12 @@ def create_tmp_snapshot(subvolume_name, subvolume_mount_point):
     return tmp_snapshot_path
 def diff(subvolume_name, subvolume_mount_point):
     tmp_snapshot_path = create_tmp_snapshot(subvolume_name, subvolume_mount_point)
+    diff_name = "tmp_diff"
     clean_snapshot_path = Snapshot.get_clean_snapshot_path(subvolume_name)
     #sh.run(f"{os.path.dirname(os.path.realpath(__file__))}/btrfs_diff.sh {clean_snapshot_path} {tmp_snapshot_path}", capture_output=False, check=False)
-    sh.run(f"{os.path.dirname(os.path.realpath(__file__))}/btrfs-snapshots-diff.py -p {clean_snapshot_path} -c {tmp_snapshot_path}", capture_output=False, check=False)
+    sh.run(f"btrfs send --no-data -p {clean_snapshot_path} {tmp_snapshot_path} > {diff_name}", capture_output=False, check=False)
+    sh.run(f"{os.path.dirname(os.path.realpath(__file__))}/btrfs-snapshots-diff.py -f {diff_name}", capture_output=False, check=False)
+    #sh.run(f"{os.path.dirname(os.path.realpath(__file__))}/btrfs-snapshots-diff.py -p {clean_snapshot_path} -c {tmp_snapshot_path}", capture_output=False, check=False)
     delete_tmp_snapshot(subvolume_name)
 
 def main():
