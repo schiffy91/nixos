a9fd813ad4fd27c440a4d2b50f04b3679cd89ca1
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-25 14:53:33 -0700

    Refactor immutability.nix and settings.nix to improve path handling and address permission issues

--- modules/settings.nix
+++ modules/settings.nix
@@ -65,6 +65,8 @@ in {
       "${config.settings.boot.pkiBundle}"
       #TODO: Some permission breaks here with home-manager
       #TODO: PermissionError: [Errno 13] Permission denied: '/home/alexanderschiffhauer/.config/kcminputrc'
+      #[alexanderschiffhauer@VM:~]$ rm -rf .config/dconf/user
+      #rm: cannot remove '.config/dconf/user': Permission denied
       #"/home/${config.settings.user.admin.username}/.config/dconf/user"
       #"/home/${config.settings.user.admin.username}/.config/gtk-3.0/settings.ini"
       #"/home/${config.settings.user.admin.username}/.config/gtk-4.0/settings.ini"
