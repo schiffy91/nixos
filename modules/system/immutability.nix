9b47ca92c0f4192ea2b8d86713502b61233eaa2b
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-28 07:14:04 -0800

    Add logging statements to files_copy function for better execution tracking

--- modules/system/immutability.nix
+++ modules/system/immutability.nix
@@ -129,6 +129,7 @@ lib.mkIf config.settings.disk.immutability.enable {
 						trace btrfs property set -ts "$path" ro false || abort "Failed to make $path read-write"
 					}
 					files_copy() {
+						log "Does it start though?"
 						local subvolume_mount_point="$1"
 						local paths_to_keep="$2"
 						local previous_snapshot="$3"
@@ -147,7 +148,7 @@ lib.mkIf config.settings.disk.immutability.enable {
 							trace test -e "$path_in_current_snapshot" && trace rm -rf "$path_in_current_snapshot"
 							trace cp -a "$path_in_previous_snapshot" "$path_in_current_snapshot"
 						done
-						return 0
+						log "Does it finish though?"
 					}
 					files_copy_rsync() {
 						log "pre-1"
