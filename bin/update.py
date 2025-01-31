8538f15ec4e0d0de5517462acd1953d16e2c9975
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-30 16:59:15 -0800

    Refactor update script to use argparse for filesystem rebuild option

--- bin/update.py
+++ bin/update.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i python3 -p python3
-import sys
+import argparse
 from nixos import Utils, Config, Shell
 
 def main():
     Utils.require_root()
     sh = Shell()
     sh.run(f"cd {Config.get_nixos_path()} && git pull")
-    rebuild_filesystem = "--rebuild_filesystem" in sys.argv
-    return Config.update(rebuild_file_system=rebuild_filesystem)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rebuild-filesystem", action="store_true", help="Whether to rebuild the filesystem.")
+    args = parser.parse_args()
+
+    return Config.update(rebuild_file_system=args.rebuild_filesystem)
 
 if __name__ == "__main__": main()
