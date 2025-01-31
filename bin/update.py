7c8e61756533d8d942646522a1cddf6c16b478f2
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-01-30 16:57:25 -0800

    Add git pull command to update script for NixOS configuration

--- bin/update.py
+++ bin/update.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i python3 -p python3
 import sys
-from nixos import Utils, Config
+from nixos import Utils, Config, Shell
 
 def main():
     Utils.require_root()
+    sh = Shell()
+    sh.run(f"cd {Config.get_nixos_path()} && git pull")
     rebuild_filesystem = "--rebuild_filesystem" in sys.argv
     return Config.update(rebuild_file_system=rebuild_filesystem)
 
