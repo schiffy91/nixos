b1e2b00176d518c780a1667b92c79f4614a8c968
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2025-02-09 10:23:19 -0800

    add --clean and --upgrade options to update.py

--- bin/update.py
+++ bin/update.py
@@ -9,8 +9,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--rebuild-filesystem", action="store_true", help="Whether to rebuild the filesystem.")
     parser.add_argument("--reboot", action="store_true", help="Whether to reboot the system after a successful update.")
+    parser.add_argument("--clean", action="store_true", help="Runs 'nix-collect-garbage -d', 'rm -rf /root/.cache/', and 'nix-store --verify --repair'")
+    parser.add_argument("--upgrade", action="store_true", help="Runs 'nix flake update'")
     args = parser.parse_args()
 
+    if args.clean: 
+        sh.run("nix-collect-garbage -d", capture_output=False)
+        sh.run("rm -rf /root/.cache")
+        sh.run("nix-store --verify --repair", capture_output=False)
+    if args.upgrade: sh.run(f"nix flake update --flake {Config.get_nixos_path()}", capture_output=False)
+    
     return Config.update(rebuild_file_system=args.rebuild_filesystem, reboot=args.reboot)
 
 if __name__ == "__main__": main()
