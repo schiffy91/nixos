0841b0c2163fee12b8bb9be78c26a57d4b677dbf
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2025-02-09 11:57:27 -0800

    Adding a patches module for bugs

---
+++ modules/system/patches.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }: {
+    systemd.package = pkgs.systemd.overrideAttrs (old: {
+        patches = old.patches ++ [ 
+            (pkgs.fetchurl {
+                url = "https://github.com/wrvsrx/systemd/compare/tag_fix-hibernate-resume%5E...tag_fix-hibernate-resume.patch";
+                hash = "sha256-iDC5lenk4LhDdU4ZRHjestYu2jXNvNZCgSCkE2hQHuQ=";
+            })
+        ];
+    })
+};
