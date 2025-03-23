fcd97fb7d2e092c5287f9d5e42097f7e6b00ee7d
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-03-23 16:37:12 -0400

    Initial support for gnome-x11 and gnome-wayland

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -1,13 +1,29 @@
 { config, pkgs, pkgs-unstable, lib, ... }: lib.mkMerge [{
-  ##### Shared Settings #####
+  ##### Shared #####
     hardware.graphics.enable = true;
-    services.displayManager.sddm.enable = lib.mkDefault true;
     xdg.portal.enable = true;
     programs.dconf.enable =  true;
     services.xserver.dpi = builtins.floor(96 * config.settings.desktop.scalingFactor);
   }
-  ##### Shared Plasma Settings #####
+  ##### Wayland #####
+  (lib.mkIf (lib.hasInfix "wayland" config.settings.desktop.environment) {
+    environment.sessionVariables.NIXOS_OZONE_WL = "1"; # https://nixos.wiki/wiki/Wayland
+    services.displayManager.sddm.wayland.enable = true;
+    services.xserver.displayManager.gdm.wayland.enable = true;
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+    ];
+  })
+  ##### X11 #####
+  (lib.mkIf (lib.hasInfix "x11" config.settings.desktop.environment) {
+    services.xserver.enable = true;
+    environment.systemPackages = with pkgs; [
+      xclip
+    ];
+  })
+  ##### Plasma #####
   (lib.mkIf (lib.hasInfix "plasma" config.settings.desktop.environment) {
+    services.displayManager.sddm.enable = lib.mkDefault true;
     services.desktopManager.plasma6 = {
       enable = lib.mkDefault true;
       enableQt5Integration = false;
@@ -18,7 +34,6 @@
       kdePackages.kaccounts-providers
       kdePackages.kaccounts-integration
       kdePackages.kio-gdrive
-      #pkgs-unstable.vulkan-hdr-layer-kwin6 # TODO Not sure if this is needed (https://discuss.kde.org/t/will-plasma-6-3-support-vk-hdr-layer/25116)
     ];
     environment.plasma6.excludePackages = (with pkgs.kdePackages; [
       kate
@@ -28,33 +43,32 @@
       print-manager
     ]);
     security.pam.services.sddm.enableKwallet = config.settings.user.admin.autoUnlockWallet.enabled;
-    ##### Scaling #####
-    environment.sessionVariables = lib.mkIf (lib.hasInfix "plasma" config.settings.desktop.environment) {
-      #"QT_AUTO_SCREEN_SCALE_FACTOR" = "1";
-      #"QT_SCALE_FACTOR" = toString config.settings.desktop.scalingFactor;
-      #"PLASMA_USE_QT_SCALING" = "1";
-    };
   })
-  ##### Plasma X11 Settings #####
+  ##### Plasma X11 #####
   (lib.mkIf (config.settings.desktop.environment == "plasma-x11") {
-    services.xserver.enable = true;
     services.displayManager.defaultSession = "plasmax11";
-    environment.systemPackages = with pkgs; [
-      xclip
-    ];
   })
-  ##### Plasma Wayland Settings #####
+  ##### Plasma Wayland #####
   (lib.mkIf (config.settings.desktop.environment == "plasma-wayland") {
-    #environment.sessionVariables.NIXOS_OZONE_WL = "1"; # https://nixos.wiki/wiki/Wayland
     services.displayManager.defaultSession = "plasma";
-    services.displayManager.sddm.wayland.enable = true;
-    environment.systemPackages = with pkgs; [
-      wl-clipboard
-    ];
+  })
+  ##### Gnome #####
+  (lib.mkIf (lib.hasInfix "gnome" config.settings.desktop.environment) {
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.displayManager.gdm.defaultSession = "gnome";
+  })
+  ##### Gnome Wayland #####
+  (lib.mkIf (config.settings.desktop.environment == "gnome-wayland") {
+    services.xserver.displayManager.gdm.wayland.enable = false;
+  })
+    ##### Gnome X11 #####
+  (lib.mkIf (config.settings.desktop.environment == "gnome-wayland") {
+    services.xserver.displayManager.gdm.wayland.enable = true;
   })
   ##### Hyprland Settings #####
   (lib.mkIf (config.settings.desktop.environment == "hyprland") {
     environment.systemPackages = with pkgs; [ xdg-desktop-portal-hyprland ];
+    services.displayManager.sddm.enable = lib.mkDefault true;
     services.displayManager.defaultSession = "hyprland";
     services.displayManager.sddm.wayland.enable = true;
     programs.hyprland = {
