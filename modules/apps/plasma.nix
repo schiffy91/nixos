9f353464489674452d81b099404e16954bec4be0
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-03-23 19:09:37 -0400

    Changes for HM for non Plasma

--- modules/apps/plasma.nix
+++ modules/apps/plasma.nix
@@ -88,4 +88,42 @@
       }
     ];
   };
+  ##### Konsole #####
+  xdg.configFile."konsolerc".text = ''
+    [Desktop Entry]
+    DefaultProfile=${settings.user.admin.username}.profile
+    [General]
+    ConfigVersion=1
+    [KonsoleWindow]
+    RememberWindowSize=false
+    ShowMenuBarByDefault=false
+    [MainWindow]
+    MenuBar=Disabled
+    StatusBar=Disabled
+    ToolBarsMovable=Disabled
+  '';
+  xdg.dataFile."konsole/${settings.user.admin.username}.profile".text = ''
+    [Cursor Options]
+    CursorShape=2
+
+    [General]
+    Name=${settings.user.admin.username}
+
+    [Keyboard]
+    KeyBindings=macos
+
+    [Scrolling]
+    HistoryMode=2
+
+    [Terminal Features]
+    BlinkingCursorEnabled=true
+
+    [KonsoleWindow]
+    RememberWindowSize=false
+    ShowMenuBarByDefault=false
+    [MainWindow]
+    MenuBar=Disabled
+    StatusBar=Disabled
+    ToolBarsMovable=Disabled
+  '';
 }
