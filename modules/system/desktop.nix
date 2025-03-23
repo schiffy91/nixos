7569b8894d807e3ae35e9102171addf5d62e762d
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-03-23 19:53:23 -0400

    Fixes for desktop environment

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -2,15 +2,20 @@
   ##### Shared #####
     hardware.graphics.enable = true;
     programs.dconf.enable =  true;
-    services.xserver.dpi = builtins.floor(96 * config.settings.desktop.scalingFactor);
-    services.displayManager.enable = true;
+    services = {
+      xserver.dpi = builtins.floor(96 * config.settings.desktop.scalingFactor);
+      displayManager.enable = true;
+      accounts-daemon.enable = true;
+    };
   }
   ##### Wayland #####
   (lib.mkIf (lib.hasInfix "wayland" config.settings.desktop.environment) {
-    environment.sessionVariables.NIXOS_OZONE_WL = "1"; # https://nixos.wiki/wiki/Wayland
-    environment.systemPackages = with pkgs; [
-      wl-clipboard
-    ];
+    environment = {
+      sessionVariables.NIXOS_OZONE_WL = "1"; # https://nixos.wiki/wiki/Wayland
+      systemPackages = with pkgs; [
+        wl-clipboard
+      ];
+    };
   })
   ##### X11 #####
   (lib.mkIf (lib.hasInfix "x11" config.settings.desktop.environment) {
@@ -21,26 +26,31 @@
   })
   ##### Plasma #####
   (lib.mkIf (lib.hasInfix "plasma" config.settings.desktop.environment) {
-    services.displayManager.sddm.wayland.enable = true;
-    services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6 = {
-      enable = lib.mkDefault true;
-      enableQt5Integration = false;
+    services = {
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+      desktopManager.plasma6 = {
+        enable = true;
+        enableQt5Integration = false;
+      };
     };
-    services.accounts-daemon.enable = true;
-    environment.systemPackages = with pkgs; [
-      kdePackages.plasma-thunderbolt
-      kdePackages.kaccounts-providers
-      kdePackages.kaccounts-integration
-      kdePackages.kio-gdrive
-    ];
-    environment.plasma6.excludePackages = (with pkgs.kdePackages; [
-      kate
-      khelpcenter
-      elisa
-      okular
-      print-manager
-    ]);
+    environment = {
+      systemPackages = with pkgs; [
+        kdePackages.plasma-thunderbolt
+        kdePackages.kaccounts-providers
+        kdePackages.kaccounts-integration
+        kdePackages.kio-gdrive
+      ];
+      plasma6.excludePackages = (with pkgs.kdePackages; [
+        kate
+        khelpcenter
+        elisa
+        okular
+        print-manager
+      ]);
+    };
     security.pam.services.sddm.enableKwallet = config.settings.user.admin.autoUnlockWallet.enabled;
   })
   ##### Plasma X11 #####
@@ -53,8 +63,12 @@
   })
   ##### Gnome #####
   (lib.mkIf (lib.hasInfix "gnome" config.settings.desktop.environment) {
-    services.displayManager.defaultSession = "gnome-xorg";
-    services.xserver.displayManager.gdm.enable = true;
+    services = {
+      displayManager.defaultSession = "gnome-xorg";
+      xserver = {
+        displayManager.gdm.enable = true;
+        desktopManager.gnome.enable = true;
+      };
   })
   ##### Gnome Wayland #####
   (lib.mkIf (config.settings.desktop.environment == "gnome-x11") {
@@ -62,20 +76,25 @@
   })
     ##### Gnome X11 #####
   (lib.mkIf (config.settings.desktop.environment == "gnome-wayland") {
-    services.displayManager.defaultSession = "gnome";
-    services.xserver.displayManager.gdm.wayland = true;
+    services = {
+      displayManager.defaultSession = "gnome";
+      xserver.displayManager.gdm.wayland = true;
+    };
   })
   ##### Hyprland Settings #####
   (lib.mkIf (config.settings.desktop.environment == "hyprland") {
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = with pkgs; [ xdg.portal.extraPortals ];
-    environment.systemPackages = with pkgs; [ xdg-desktop-portal-hyprland ];
-    services.displayManager.sddm.enable = lib.mkDefault true;
-    services.displayManager.defaultSession = "hyprland";
-    services.displayManager.sddm.wayland.enable = true;
-    programs.hyprland = {
+    xdg.portal = {
       enable = true;
-      package = pkgs-unstable.hyprland; # Always use unstable for hyprland given its bugs & pace of development
+      extraPortals = with pkgs; [ xdg.portal.extraPortals ];
     };
+    environment.systemPackages = with pkgs; [ xdg-desktop-portal-hyprland ];
+    services.displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+      defaultSession = "hyprland";
+    };
+    programs.hyprland.enable = true;
   })
 ]
