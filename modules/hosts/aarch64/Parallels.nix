9cbd4ed48b887544fca635f8ffdffb968dea4527
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-03-30 15:58:56 -0700

    Adding a Parallels config. Probably bokren

---
+++ modules/hosts/aarch64/Parallels.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+let
+  kernel_version = "6_14";
+  kernelPackage = "linux_${kernel_version}";
+  linuxPackage = "linuxPackages_${kernel_version}";
+in
+{
+  ##### Host Name #####
+  networking.hostName = "VM";
+  ##### Disk Information #####
+  settings.disk.device = "/dev/sda";
+  settings.disk.encryption.enable = false;
+  settings.disk.swap.enable = false;
+  settings.disk.immutability.enable = true;
+  ##### Desktop #####
+  settings.desktop.environment = lib.mkForce "gnome-wayland";
+  ##### Parallels #####
+  hardware.parallels = {
+    enable = true;
+    package = pkgs.linuxKernel.packages.${kernelPackage}.prl-tools;
+  };
+  boot.kernelPackages = pkgs.${linuxPackage};
+  ##### Boot Configuration #####
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "sr_mod"
+  ];
+  ##### Security #####
+  security.sudo.wheelNeedsPassword = false;
+  ##### Packages #####
+  environment.systemPackages = with pkgs; [
+      linuxKernel.packages.${kernelPackage}.prl-tools
+  ];
+  ##### Security #####
+  security.sudo.wheelNeedsPassword = false;
+  settings.user.admin.autoLogin.enable = true;
+  settings.user.admin.autoLock.enable = false;
+  ##### Packages #####
+  environment.systemPackages = with pkgs; [
+    spice-vdagent
+    spice-protocol
+  ];
+  ##### Networking #####
+  settings.networking.lanSubnet = "192.168.64.0/24";
+}
