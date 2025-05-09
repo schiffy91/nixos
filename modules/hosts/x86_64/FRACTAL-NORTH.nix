331e050002cae2265917c895274a9e84754ace92
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2025-05-09 13:32:02 -0700

    Change to gnome

--- modules/hosts/x86_64/FRACTAL-NORTH.nix
+++ modules/hosts/x86_64/FRACTAL-NORTH.nix
@@ -59,7 +59,7 @@
     };
   };
   ##### Desktop Environment #####
-  settings.desktop.environment = "plasma-wayland";
+  settings.desktop.environment = "gnome-wayland";
   ##### Packages #####
   environment.systemPackages = with pkgs; [
     google-chrome
