457b000fa76de2437aad64adc42f56487f1787f0
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2025-05-09 13:25:46 -0700

    Removing ollama since unused and balloons compile time

--- modules/hosts/x86_64/FRACTAL-NORTH.nix
+++ modules/hosts/x86_64/FRACTAL-NORTH.nix
@@ -92,7 +92,4 @@
   settings.networking.ports.udp = (lib.range 47998 48000) ++ (lib.range 8000 8010);
   ##### Thunderbolt #####
   services.hardware.bolt.enable = true;
-  ##### Ollama #####
-  services.nextjs-ollama-llm-ui.enable = true;
-  services.ollama.enable = true;
 }
