10dcaa671b99e53d8ab2b5e7b0afeacbd7285c4d
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2026-06-05 16:52:05 -0700

    Install BTRC language server with VS Code

--- nix/apps/vscode.nix
+++ nix/apps/vscode.nix
@@ -1,6 +1,10 @@
-{ config, lib, pkgs-unstable, ... }:
+{ config, inputs, lib, pkgs, pkgs-unstable, ... }:
 let
+  system = pkgs.stdenv.hostPlatform.system;
   user = config.settings.users.admin.username;
 in lib.mkIf (config.settings.apps.enable && config.settings.apps.vscode.enable) {
-  users.users.${user}.packages = [ pkgs-unstable.vscode ];
+  users.users.${user}.packages = [
+    pkgs-unstable.vscode
+    inputs.btrc.packages.${system}.btrc-lsp
+  ];
 }
