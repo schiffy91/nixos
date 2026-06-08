e9f31620414b5b6bcaad879d2cf28fa0e276660f
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2026-06-07 21:22:30 -0700

    Use mutable VSCode extension directory

--- nix/apps/dev/vscode.nix
+++ nix/apps/dev/vscode.nix
@@ -3,13 +3,14 @@ let
   system = pkgs.stdenv.hostPlatform.system;
   user = config.settings.users.admin.username;
   btrc = inputs.btrc.packages.${system};
-  vscode = pkgs-unstable.vscode-with-extensions.override {
-    vscode = pkgs-unstable.vscode;
-    vscodeExtensions = [ btrc.btrc-vscode ];
-  };
 in lib.mkIf (config.settings.apps.enable && config.settings.apps.dev.enable && config.settings.apps.vscode.enable) {
-  users.users.${user}.packages = [
-    vscode
-    btrc.btrc-lsp
-  ];
+  home-manager.users.${user} = {
+    home.packages = [ btrc.btrc-lsp ];
+    programs.vscode = {
+      enable = true;
+      package = pkgs-unstable.vscode;
+      mutableExtensionsDir = true;
+      profiles.default.extensions = [ btrc.btrc-vscode ];
+    };
+  };
 }
