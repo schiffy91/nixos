a6c20f63ad1d62cc212959573d2b63ba1a2ee7eb
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2026-09-01 20:40:06 -0700

    feat(apps): persist BTRSmith state

--- nix/apps/gaming/btrsmith.nix
+++ nix/apps/gaming/btrsmith.nix
@@ -2,6 +2,7 @@
 let
   enabled = config.settings.apps.enable && config.settings.apps.gaming.enable && config.settings.apps.btrsmith.enable;
   system = pkgs.stdenv.hostPlatform.system;
+  home = "/home/${config.settings.users.admin.username}";
   inputAvailable = inputs ? btrsmith && inputs.btrsmith ? packages && builtins.hasAttr system inputs.btrsmith.packages && inputs.btrsmith.packages.${system} ? btrsmith;
 in lib.mkIf enabled {
   assertions = [{
@@ -9,4 +10,5 @@ in lib.mkIf enabled {
     message = "settings.apps.btrsmith.enable requires an exact BTRSmith flake input with packages.${system}.btrsmith";
   }];
   environment.systemPackages = lib.optionals inputAvailable [ inputs.btrsmith.packages.${system}.btrsmith ];
+  settings.disk.immutability.persist.paths = [ "${home}/.local/state/com.schiffy91.btrsmith" ];
 }
