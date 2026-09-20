a092b8ee9d211bfba1a7b192223d2c0d3149731d
Alexander Schiffhauer <Alexander.Schiffhauer@gmail.com>
2026-09-20 11:00:47 -0700

    Revert "Add DLSS 5 neural rendering as a removable module with a tray toggle"
    This reverts commit b90fb66b156361b7efd39e551946b983b35cb011.

--- nix/apps/gaming/steam/default.nix
+++ nix/apps/gaming/steam/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, protonCustom, dlss5, ... }:
+{ config, pkgs, lib, protonCustom, ... }:
 let
   user = config.settings.users.admin.username;
   home = "/home/${user}";
@@ -10,7 +10,6 @@ let
   defaultLaunchPrefix = "PROTON_ENABLE_WAYLAND=1 PROTON_ENABLE_HDR=1 DXVK_HDR=1 ENABLE_HDR_WSI=1";
   games = import ./games.nix {
     inherit protonCustomName chromiumDpi;
-    dlss5LaunchEnv = dlss5.launchEnv;
     rsSampleSize = config.settings.rocksmith.sampleSize;
     rsSampleRate = config.settings.rocksmith.sampleRate;
   };
